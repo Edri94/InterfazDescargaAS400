1f237b5bf963505d7d7386f2dce2a25535ab6aed
Edri94 <edrialan.angulo@bbva.com>
2022-02-18 13:21:13 -0600

    first commit

---
+++ InterfazDescargaAS400/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfazDescargaAS400
+{
+    public class Class1
+    {
+    }
+}
