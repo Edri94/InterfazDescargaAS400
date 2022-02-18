8924baba5ef06f7e0a6ae2ee5cee914877048dfe
Edri94 <edrialan.angulo@bbva.com>
2022-02-18 14:16:02 -0600

    Se agregaron las funciones base para la interfaz

---
+++ InterfazDescargaAS400/Data/ArchivoTexto.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfazDescargaAS400.Data
+{
+    public class ArchivoTexto
+    {
+        String path;
+        public ArchivoTexto(String path)
+        {
+            this.path = path;
+        }
+
+        public String leerArchivo()
+        {
+            String line;
+
+            try
+            {
+                StreamReader sr = new StreamReader(path);
+                line = sr.ReadLine();
+                while (line != null)
+                {
+                    Console.WriteLine(line);
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch (IOException ex)
+            {
+
+                throw;
+            }
+
+            return "";
+        }
+
+        public bool existeArchivo()
+        {
+            bool existe;
+
+            try
+            {
+                existe = File.Exists(path);
+            }
+            catch (Exception ex)
+            {
+                existe = false;
+            }
+
+            return existe;
+        }
+
+    }
+}
+
